6730b9f87dcbbe87b5906747c34b3eedd2a6cab1
Austin <austinih46@gmail.com>
2023-07-18 20:27:58 -0500

    refactored platform and genre hooks

--- game-hub/src/hooks/useGenres.ts
+++ game-hub/src/hooks/useGenres.ts
@@ -1,3 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClients from "../services/api-clients";
+import { FetchResponse } from "./useData";
 import genres from "../data/genres";
 
 export interface Genre {
@@ -6,7 +9,14 @@ export interface Genre {
   image_background: string;
 }
 
-
-const useGenres = () => ({data: genres, isLoading: false, error: null})
+const useGenres = () => useQuery({
+  queryKey: ['genres'],
+  queryFn: () => 
+    apiClients
+      .get<FetchResponse<Genre>>('/genres')
+      .then(res => res.data),
+  staleTime: 24 * 60 * 60 * 1000, //24h
+  initialData: { count:  genres.length, results: genres }
+})
 
 export default useGenres
--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -4,15 +4,19 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import theme from "./theme.ts";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <QueryClientProvider client={queryClient}>
         <App />
-      </ChakraProvider>
-    </BrowserRouter>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
