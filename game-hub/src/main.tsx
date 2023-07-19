b3f54c4e11689a0d6df92b0ccc810e80ceaf62f7
Austin <austinih46@gmail.com>
2023-07-19 15:55:00 -0500

    Removed devtools for deployment

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 import theme from "./theme.ts";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
